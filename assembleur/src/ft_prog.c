f5e6b84256b75a28c6da0532f47366ffa98135ef
Jerome DESMARES <jdesmare@e1r6p13.42.fr>
2017-02-25 09:56:44 +0100

    ft_clear_space + write_code ok, en attente du parser pour finaliser l'assembleur

--- assembleur/src/ft_prog.c
+++ assembleur/src/ft_prog.c
@@ -6,52 +6,11 @@
 /*   By: jdesmare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/23 15:34:11 by jdesmare          #+#    #+#             */
-/*   Updated: 2017/02/24 19:29:34 by jdesmare         ###   ########.fr       */
+/*   Updated: 2017/02/25 08:56:34 by jdesmare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
-
-static int		get_code(char c)
-{
-	if (c == 'r')
-		return (REG_CODE);
-	else if (c == DIRECT_CHAR)
-		return (DIR_CODE);
-	else if (c == '\0')
-		return (0);
-	else
-		return (IND_CODE);
-}
-
-static void		write_octcode(t_struct *env, char *line)
-{
-	int		i;
-	int		count;
-
-	i = 0;
-	count = 0;
-	while (!ft_isspace(line[i]) && line[i] != DIRECT_CHAR)
-		i++;
-	while (ft_isspace(line[i]))
-		i++;
-	count += get_code(line[i]);
-	while (line[i] != SEPARATOR_CHAR && line[i] != '\0')
-		i++;
-	while ((line[i] == SEPARATOR_CHAR || ft_isspace(line[i])) && line[i] != '\0')
-		i++;
-	count = count << 2;
-	count += get_code(line[i]);
-	while (line[i] != SEPARATOR_CHAR && line[i] != '\0')
-		i++;
-	while ((line[i] == SEPARATOR_CHAR || ft_isspace(line[i])) && line[i] != '\0')
-		i++;
-	count = count << 2;
-	count += get_code(line[i]);
-	count = count << 2;
-	put_hex_in_char(env, count, env->i);
-	env->i++;
-}
 
 static int		write_opcode(t_struct *env, char *line)
 {
@@ -116,6 +75,9 @@ static char		*del_comments(char *line)
 
 int				ft_prog(t_struct *env, char *line)
 {
+	int		current_pos;
+
+	current_pos = env->i;
 	line = del_comments(line);
 	line = find_op(line);
 	if (line == NULL)
@@ -124,6 +86,10 @@ int				ft_prog(t_struct *env, char *line)
 	if (ft_strncmp("fork", line, 4) != 0 && ft_strncmp("lfork", line, 5) != 0 &&
 		ft_strncmp("zjmp", line, 4) != 0 && ft_strncmp("live", line, 4) != 0)
 		write_octcode(env, line);
-//	write_params(env, line);
+	while (!ft_isspace(*line) && *line != DIRECT_CHAR)
+		line++;
+	while (ft_isspace(*line))
+		line++;
+	write_params(env, line, current_pos);
 	return (1);
 }
