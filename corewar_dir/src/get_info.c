90ca40f86190a24f1ff883a4bc8e35a0d5aa199c
Jimmy GONCALVES <jgoncalv@e1r5p11.42.fr>
2017-02-26 19:05:25 +0100

    oups

--- corewar_dir/src/get_info.c
+++ corewar_dir/src/get_info.c
@@ -11,43 +11,7 @@
 /* ************************************************************************** */
 
 #include "corewar.h"
-/*
-static char		*ft_read_info(int fd, int size)
-{
-	char	*buf;
 
-	if (!(buf = ft_strnew(size)))
-		return (NULL);
-	if (read(fd, buf, size) == -1)
-	{
-		ft_strdel(&buf);
-		ft_putstr_fd("Error: Fail to read the file.\n", 2);
-		return (NULL);
-	}
-	return (buf);
-}
-
-static int	get_champ_info(int fd, t_info *champ)
-{
-	char	*str;
-
-	if (!(str = ft_read_info(fd, 4)))
-		return (0);
-	else
-		ft_strdel(&str);
-	if (!(champ->name = ft_read_info(fd, PROG_NAME_LENGTH + 4)))
-		return (0);
-	if (!(str = ft_read_info(fd, PROG_LENGTH_LENGTH)))
-		return (0);
-	champ->prog_len = check_prog_len(str);
-	ft_strdel(&str);
-	if (!(champ->comment = ft_read_info(fd, COMMENT_LENGTH + 4)))
-		return (0);
-	if (!(champ->prog = ft_read_info(fd, champ->prog_len)))
-		return (0);
-	return (1);
-}
-*/
 static int	get_champ_info(int fd, t_info *champ)
 {
 	char	buf[HEADER_LENGTH + CHAMP_MAX_SIZE];
