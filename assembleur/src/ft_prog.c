8fa97a47de4a67c6b58873935428f1fd4b1fb8c2
Jerome DESMARES <jdesmare@e1r6p13.42.fr>
2017-02-27 14:41:50 +0100

    I want to make free

--- assembleur/src/ft_prog.c
+++ assembleur/src/ft_prog.c
@@ -6,7 +6,7 @@
 /*   By: jdesmare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/23 15:34:11 by jdesmare          #+#    #+#             */
-/*   Updated: 2017/02/27 13:48:23 by jdesmare         ###   ########.fr       */
+/*   Updated: 2017/02/27 14:41:07 by jdesmare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,44 @@
 static int		write_opcode(t_struct *env, char *line)
 {
 	int		i;
-	char	*find;
+	char	*op;
 
+	i = 0;
+	op = ft_strdup(line);
+	while (!ft_isspace(op[i]))
+		i++;
+	op[i] = '\0';
+	ft_printf("op = |%s|\n", op);
 	i = 0;
 	while (g_op_tab[i].name)
 	{
-		find = ft_strstr(line, g_op_tab[i].name);
-		if (find != NULL && *(find - 2) != '%' &&
-				!ft_isalpha(find[ft_strlen(g_op_tab[i].name)]))
+		if (ft_strcmp(op, g_op_tab[i].name) == 0)
 		{
 			put_hex_in_char(env, g_op_tab[i].opcode, env->i, 1);
 			env->i++;
+			free(op);
 			return (1);
 		}
 		i++;
 	}
+	free(op);
 	return (1);
 }
 
 static char		*find_op(char *line)
 {
 	int		i;
-	char	*find;
 
 	i = 0;
 	while (valid_char(line[i]) > 0)
 		i++;
 	if (line[i] == LABEL_CHAR)
 		line += i + 1;
-	i = -1;
-	while (g_op_tab[++i].name)
-	{
-		find = ft_strstr(line, g_op_tab[i].name);
-		if (find != NULL && !ft_isalpha(find[ft_strlen(g_op_tab[i].name)]) &&
-				line[find - line - 2] != '%')
-			return (find);
-		else if (find != NULL && line[find - line - 2] != '%' &&
-				!ft_isalpha(find[ft_strlen(g_op_tab[i].name)]))
-		{
-			line = find;
-			find = find_op(find + 1);
-			if (find != NULL)
-				return (find);
-		}
-	}
-	return (find);
+	while (ft_isspace(*line))
+		line++;
+	if (*line == '\0')
+		return (NULL);
+	return (line);
 }
 
 static char		*del_comments(char *line)
