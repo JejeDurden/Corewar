f94e8e4d330b5dacdc8a653890969088a61bbcd7
Raphael GHIRELLI <rghirell@e1r6p11.42.fr>
2017-03-06 19:50:40 +0100

    reglages

--- corewar_dir/src/write_params.c
+++ corewar_dir/src/write_params.c
@@ -6,7 +6,7 @@
 /*   By: rghirell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/06 18:01:27 by rghirell          #+#    #+#             */
-/*   Updated: 2017/03/06 19:02:49 by rghirell         ###   ########.fr       */
+/*   Updated: 2017/03/06 19:50:09 by rghirell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ void	write_params(t_struct *env, t_process *proc, unsigned int dest, int i)
 
 	j = search_champ_id(env, proc);
 	ft_memset(env->map_color + dest, j + 49, REG_SIZE);
-	put_octet(env, dest, 0, i - 1);
+	put_octet(env, dest, 0, proc->reg[i - 1]);
 }
